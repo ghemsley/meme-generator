4cb758a5683617c8ee46e376c7d034774e34e690
ghemsley <ghemsley@protonmail.ch>
2021-01-27 02:26:10 -0600

    Add '/users/:id' route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,12 @@
 require './config/environment'
 class UserController < ApplicationController
+  get '/users/:id' do
+    @user ||= User.find_by_id(params[:id])
+    if @user
+      erb :'users/show'
+    else
+      flash[:show_user_error] = 'Error: Failed to display account page'
+      redirect '/signin'
+    end
+  end
 end
