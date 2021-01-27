ce0d1dc2211888ad13e0bb41b8859e474f4ecf5b
ghemsley <ghemsley@protonmail.ch>
2021-01-27 17:11:22 -0600

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 require './config/environment'
-
+require 'sinatra/flash'
+require 'securerandom'
 class ApplicationController < Sinatra::Base
-  register(Sinatra::JS)
   configure do
+    set :server, :puma
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions
+    use Rack::Session::Cookie, key: 'rack.session',
+                               path: '/',
+                               secret: SecureRandom.hex(64)
+    register(Sinatra::JS)
+    register(Sinatra::Flash)
   end
 
   get '/' do
@@ -14,10 +19,19 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def signed_in?
-      if session[:user_id]
+      if session[:user_id] || @user
         true
       else
         false
+      end
+    end
+
+    def current_user
+      if @user
+        @user
+      elsif session[:user_id]
+        @user ||= User.find_by_id(session[:user_id])
+        @user
       end
     end
   end
