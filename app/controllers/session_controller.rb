a65f77e1d4f9aea3fb12b6c4fa47e09e0de76657
ghemsley <ghemsley@protonmail.ch>
2021-01-27 17:12:07 -0600

    Refactor session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,11 +1,16 @@
 require './config/environment'
 
 class SessionController < ApplicationController
+  get '/signup' do
+    erb :'sessions/signup'
+  end
+
   post '/signup' do
     username = params[:user][:username]
     password = params[:user][:password]
     user = User.new(username: username, password: password)
     if user.save
+      flash[:success] = 'Success: Your account has been created'
       redirect '/signin'
     else
       flash[:signup_error] = 'Error: Failed to create user'
@@ -14,7 +19,7 @@ class SessionController < ApplicationController
   end
 
   get '/signin' do
-    erb :signin
+    erb :'sessions/signin'
   end
 
   post '/signin' do
@@ -23,20 +28,21 @@ class SessionController < ApplicationController
     user = User.find_by(username: username)
     if user&.authenticate(password)
       session[:user_id] = user.id
+      flash[:success] = 'Success: You have been signed in!'
       redirect "/users/#{user.id}"
     else
-      flash[:signin_error] = "Error: Failed to authenticate"
+      flash[:error] = 'Error: Failed to authenticate'
       redirect '/signin'
     end
   end
 
   get '/signout' do
     if session[:user_id]
-      session[:user_id].delete
-      flash[:signout_success] = "Success: You have been signed out"
+      session.delete(:user_id)
+      flash[:success] = 'Success: You have been signed out!'
       redirect '/signin'
     else
-      flash[:signout_error] = "Error: You are already signed out!"
+      flash[:error] = 'Error: You are already signed out'
       redirect 'signin'
     end
   end
