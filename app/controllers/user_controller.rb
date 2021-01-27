a7dae8e4dcd522eb044729bdcc6d27b19e0a18b3
ghemsley <ghemsley@protonmail.ch>
2021-01-27 17:12:27 -0600

    Refactor user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,17 @@
 require './config/environment'
 class UserController < ApplicationController
   get '/users/:id' do
-    if session[:user_id] == params[:id]
-      @user ||= User.find_by_id(params[:id])
-      if @user 
+    if session[:user_id] == params[:id].to_i
+      @user ||= User.find_by_id(session[:user_id])
+      if @user
+        @memes = User.find_by_id(1).memes.all
         erb :'users/show'
       else
-        flash[:find_user_error] = "Error: Failed to find user with id #{params[:id]}"
+        flash[:error] = "Error: Failed to find user with id #{params[:id]}"
+        redirect '/signin'
       end
     else
-      flash[:show_user_error] = 'Error: Insufficent authorization to view this page'
+      flash[:error] = 'Error: Insufficent authorization to view this page'
       redirect '/signin'
     end
   end
