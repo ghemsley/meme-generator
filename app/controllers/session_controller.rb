7a2885143f25a4130caeefdda0964434100381e8
ghemsley <ghemsley@protonmail.ch>
2021-01-27 01:52:12 -0600

    Add routes to session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,23 @@
 require './config/environment'
 
 class SessionController < ApplicationController
+  post '/signup' do
+    username = params[:user][:username]
+    password = params[:user][:password]
+    user = User.new(username: username, password: password)
+    if user.save
+      redirect '/signin'
+    else
+      flash[:signup_error] = 'Error: Failed to create user'
+      redirect '/signup'
+    end
+  end
+
+  get '/signin' do
+    erb :signin
+  end
+
+  post '/signin' do
+    redirect '/account'
+  end
 end
