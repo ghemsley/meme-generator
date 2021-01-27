b4411a6e6d50e68340d243920ff6ad44d8aa31d7
ghemsley <ghemsley@protonmail.ch>
2021-01-27 02:25:44 -0600

    Add signin and signout routes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -18,6 +18,25 @@ class SessionController < ApplicationController
   end
 
   post '/signin' do
-    redirect '/account'
+    username = params[:user][:username]
+    password = params[:user][:password]
+    user = User.find_by(username: username)
+    if user&.authenticate(password)
+      redirect "/users/#{user.id}"
+    else
+      flash[:signin_error] = "Error: Failed to authenticate"
+      redirect '/signin'
+    end
+  end
+
+  get '/signout' do
+    if session[:user_id]
+      session[:user_id].delete
+      flash[:signout_success] = "Success: You have been signed out"
+      redirect '/signin'
+    else
+      flash[:signout_error] = "Error: You are already signed out!"
+      redirect 'signin'
+    end
   end
 end
