f77336b2b26d1d1c46954f50346d09a7d9db4671
ghemsley <ghemsley@protonmail.ch>
2021-01-28 20:18:25 -0600

    Add webpack and dom-to-image-more

---
+++ src/js/domToImage.js
@@ -0,0 +1,70 @@
+import domtoimage from 'dom-to-image-more'
+
+global.formSubmit = (event) => {
+  event.preventDefault()
+
+  let outerContainer = document.createElement('div')
+  let innerContainer = document.createElement('div')
+  let image = document.createElement('img')
+  let topCaptionP = document.createElement('p')
+  let bottomCaptionP = document.createElement('p')
+
+  let reader = new FileReader()
+  let name = event.target[0].value
+  let file = event.target[3].files[0]
+  let topCaption = event.target[1].value
+  let bottomCaption = event.target[2].value
+
+  innerContainer.className = 'image-container'
+  image.className = 'meme-image'
+  topCaptionP.className = 'top-caption'
+  bottomCaptionP.className = 'bottom-caption'
+  topCaptionP.innerHTML = topCaption
+  bottomCaptionP.innerHTML = bottomCaption
+
+  reader.addEventListener('load', (event) => {
+    image.src = event.target.result
+  })
+  reader.readAsDataURL(file)
+
+  innerContainer.appendChild(topCaptionP)
+  innerContainer.appendChild(image)
+  innerContainer.appendChild(bottomCaptionP)
+  outerContainer.appendChild(innerContainer)
+  document.querySelector('.layout').appendChild(outerContainer)
+
+  const buttonSubmit = () => {
+    domtoimage.toPng(innerContainer).then(function (dataUrl) {
+      let image2 = new Image()
+      image2.src = dataUrl
+      document.querySelector('.layout').appendChild(image2)
+      fetch(dataUrl)
+        .then((response) => {
+          return response.blob()
+        })
+        .then((file) => {
+          console.log(file)
+          const formData = new FormData()
+          formData.append('meme[name]', name)
+          formData.append('meme[top_caption]', topCaption)
+          formData.append('meme[bottom_caption]', bottomCaption)
+          formData.append('meme[image]', file, name + '.png')
+          fetch('/memes', {
+            method: 'POST',
+            body: formData
+          })
+            .then((response) => console.log(response))
+            .catch(function (error) {
+              console.error('Error: Failed to save image to server', error)
+            })
+        })
+    })
+  }
+
+  let button = document.createElement('button')
+  button.innerHTML = 'Save meme'
+  button.onclick = buttonSubmit
+  document.querySelector('.layout').appendChild(button)
+
+  return false
+}
