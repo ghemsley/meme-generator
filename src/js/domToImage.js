e298bb00b22a8cbe18e25f461d1adc73d86a7a60
ghemsley <ghemsley@protonmail.ch>
2021-01-29 16:23:11 -0600

    Fix bug in domtoimage.js

--- src/js/domToImage.js
+++ src/js/domToImage.js
@@ -1,53 +1,19 @@
 import domtoimage from 'dom-to-image-more'
 
 global.formSubmit = (event) => {
-  const buttonSubmit = () => {
-    domtoimage.toPng(innerContainer).then(function (dataUrl) {
-      let image2 = new Image()
-      image2.src = dataUrl
-      fetch(dataUrl)
-        .then((response) => {
-          return response.blob()
-        })
-        .then((file2) => {
-          const formData = new FormData()
-          formData.append('meme[name]', name)
-          formData.append('meme[top_caption]', topCaption)
-          formData.append('meme[bottom_caption]', bottomCaption)
-          formData.append('meme[image]', file2, name + '.png')
-          formData.append('meme[original_image]', file, name + '_original.png')
-          fetch('/memes', {
-            method: 'POST',
-            body: formData
-          })
-            .then((response) => console.log(response))
-            .then(() => {
-              window.location.href = `/users/${user_id}/memes`
-            })
-            .catch(function (error) {
-              console.error('Error: Failed to save image to server', error)
-            })
-        })
-    })
-  }
-
   event.preventDefault()
-  
-  if (document.querySelector('.outer-container') != null) {
-    document.querySelector('.outer-container').remove()
-    document.querySelector('.form-container form button')
-  }
-  let outerContainer = document.createElement('div')
-  let innerContainer = document.createElement('div')
-  let image = document.createElement('img')
-  let topCaptionP = document.createElement('p')
-  let bottomCaptionP = document.createElement('p')
 
-  let reader = new FileReader()
-  let name = event.target[0].value
-  let file = event.target[3].files[0]
-  let topCaption = event.target[1].value
-  let bottomCaption = event.target[2].value
+  var outerContainer = document.createElement('div')
+  var innerContainer = document.createElement('div')
+  var image = document.createElement('img')
+  var topCaptionP = document.createElement('p')
+  var bottomCaptionP = document.createElement('p')
+
+  var reader = new FileReader()
+  var name = event.target[0].value
+  var file = event.target[3].files[0]
+  var topCaption = event.target[1].value
+  var bottomCaption = event.target[2].value
 
   outerContainer.className = 'outer-container'
   innerContainer.className = 'inner-container'
@@ -62,16 +28,62 @@ global.formSubmit = (event) => {
   })
   reader.readAsDataURL(file)
 
-  let button = document.createElement('button')
-  button.innerHTML = 'Save meme'
-  button.className = 'submit-button'
-  button.onclick = buttonSubmit
-  document.querySelector('.form-container form').appendChild(button)
   innerContainer.appendChild(topCaptionP)
   innerContainer.appendChild(image)
   innerContainer.appendChild(bottomCaptionP)
   outerContainer.appendChild(innerContainer)
   document.querySelector('.layout').appendChild(outerContainer)
+
+  const buttonSubmit = () => {
+    domtoimage.toPng(innerContainer).then(function (dataUrl) {
+      console.log(dataUrl)
+      var image2 = new Image()
+      image2.src = dataUrl
+      fetch(dataUrl)
+        .then((response) => {
+          console.log(response)
+          return response.blob()
+        })
+        .then((file2) => {
+          console.log(file.type)
+          console.log(file2)
+          console.log(file2.type)
+
+          const formData = new FormData()
+          formData.append('meme[name]', name)
+          formData.append('meme[top_caption]', topCaption)
+          formData.append('meme[bottom_caption]', bottomCaption)
+          formData.append(
+            'meme[image]',
+            file2,
+            name + `.${file2.type.split('/').pop()}`
+          )
+          formData.append(
+            'meme[original_image]',
+            file,
+            name + `_original.${file.type.split('/').pop()}`
+          )
+          fetch('/memes', {
+            method: 'POST',
+            body: formData
+          })
+            .then((response) => console.log(response))
+            .then(() => {
+              window.location.href = `/users/${user_id}/memes`
+            })
+            .catch(function (error) {
+              console.error('error', error)
+            })
+        })
+    })
+  }
+
+  var button = document.createElement('button')
+  button.innerHTML = 'Save meme'
+  button.className = 'submit-button'
+  button.onclick = buttonSubmit
+
+  document.querySelector('.form-container form').appendChild(button)
 
   return false
 }
