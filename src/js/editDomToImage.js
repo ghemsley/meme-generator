70b8fe3cb2acfdd7676c6c9961cb9e953083c3d6
ghemsley <ghemsley@protonmail.ch>
2021-01-30 01:32:10 -0600

    Add editDomToImage.js, update webpack config

---
+++ src/js/editDomToImage.js
@@ -0,0 +1,88 @@
+import domtoimage from 'dom-to-image-more'
+
+var outerContainer = document.createElement('div')
+var innerContainer = document.createElement('div')
+var image = document.createElement('img')
+var topCaptionP = document.createElement('p')
+var bottomCaptionP = document.createElement('p')
+
+var button = document.querySelector('.create-button')
+
+var reader = new FileReader()
+
+var name
+var file
+var topCaption
+var bottomCaption
+
+outerContainer.className = 'outer-container'
+innerContainer.className = 'inner-container'
+image.className = 'meme-image'
+topCaptionP.className = 'top-caption'
+bottomCaptionP.className = 'bottom-caption'
+
+global.formSubmit = (event) => {
+  event.preventDefault()
+  button.style = { display: 'inline-block' }
+  name = event.target[0].value
+  topCaption = event.target[1].value
+  bottomCaption = event.target[2].value
+
+  if (event.target[3].files[0] != null && event.target[3].files[0] != 'undefined') {
+    reader.addEventListener('load', (event) => {
+      image.src = event.target.result
+    })
+    file = event.target[3].files[0]
+    reader.readAsDataURL(file)
+  } else {
+    reader.addEventListener('load', (event) => {
+      image.src = originalImageSrc
+    })
+    file = event.target[3].files[0]
+    reader.readAsDataURL(file)
+  }
+
+  topCaptionP.innerHTML = topCaption
+  bottomCaptionP.innerHTML = bottomCaption
+  innerContainer.appendChild(topCaptionP)
+  innerContainer.appendChild(image)
+  innerContainer.appendChild(bottomCaptionP)
+  outerContainer.appendChild(innerContainer)
+  document.querySelector('.layout').appendChild(outerContainer)
+  return false
+}
+
+global.buttonSubmit = () => {
+  domtoimage.toPng(innerContainer).then(function (dataUrl) {
+    console.log(dataUrl)
+    var image2 = new Image()
+    image2.src = dataUrl
+    fetch(dataUrl)
+      .then((response) => {
+        console.log(response)
+        return response.blob()
+      })
+      .then((file2) => {
+        const formData = new FormData()
+        formData.append('meme[name]', name)
+        formData.append('meme[top_caption]', topCaption)
+        formData.append('meme[bottom_caption]', bottomCaption)
+        formData.append(
+          'meme[image]',
+          file2,
+          name + `.${file2.type.split('/').pop()}`
+        )
+        fetch('/memes', {
+          method: 'PATCH',
+          body: formData
+        })
+          .then((response) => console.log(response))
+          .then(() => {
+            window.location.href = `/users/${user_id}/memes`
+          })
+          .catch(function (error) {
+            console.error('error', error)
+          })
+      })
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: { domToImage: './src/js/domToImage.js' },
+  entry: { domToImage: './src/js/domToImage.js', editDomToImage: './src/js/editDomToImage.js' },
   output: {
     path: path.resolve(__dirname, 'public/js'),
     filename: '[name].bundle.js',
