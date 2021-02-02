abc06ee3cde2beac3aabf877fc6ad763709dc525
ghemsley <ghemsley@protonmail.ch>
2021-02-02 14:25:59 -0600

    Add style-loader, css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,22 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: { domToImage: './src/js/domToImage.js', editDomToImage: './src/js/editDomToImage.js', tinySlider: './src/js/tinySlider.js' },
+  entry: {
+    domToImage: './src/js/domToImage.js',
+    editDomToImage: './src/js/editDomToImage.js',
+    tinySlider: './src/js/tinySlider.js'
+  },
   output: {
     path: path.resolve(__dirname, 'public/js'),
-    filename: '[name].bundle.js',
-  }
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  watch: true
 }
