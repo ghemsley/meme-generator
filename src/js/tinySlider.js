ba4197989f4d1f4947adb301dd52d093abf4a6d4
ghemsley <ghemsley@protonmail.ch>
2021-02-02 20:03:11 -0600

    Customize tinyslider options

--- src/js/tinySlider.js
+++ src/js/tinySlider.js
@@ -1,21 +1,20 @@
 import { tns } from 'tiny-slider/src/tiny-slider'
 
-document.addEventListener(
-  'DOMContentLoaded',
-  () => {
-    var slider = tns({
-      container: '.slider',
-      items: 3,
-      slideBy: 1,
-      autoWidth: true,
-      gutter: 16,
-      slideBy: 1,
-      center: true,
-      controlsPosition: 'bottom',
-      arrowKeys: true,
-      nav: false,
-      autoplayButton: false,
-    })
-  },
-  false
-)
+var slider = tns({
+  container: '.slider',
+  items: 3,
+  slideBy: 1,
+  autoplay: true,
+  autoWidth: true,
+  gutter: 16,
+  slideBy: 1,
+  center: true,
+  controlsPosition: 'bottom',
+  arrowKeys: true,
+  nav: false,
+  autoplayButton: false,
+  autoplayTimeout: 3000,
+  autoplayPosition: 'bottom',
+  autoplayHoverPause: true,
+  controlsText: ['Previous', 'Next']
+})
