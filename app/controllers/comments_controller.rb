24c7793877c73d410e68340160e0027ae74e1435
ghemsley <ghemsley@protonmail.ch>
2021-02-03 10:06:17 -0600

    Add delete route to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 require './config/environment'
 
 class CommentsController < ApplicationController
-  post '/comments' do 
+  post '/comments' do
     if signed_in?
       user = current_user
       if user && user.id == params[:comment][:user_id].to_i
@@ -10,18 +10,38 @@ class CommentsController < ApplicationController
         comment.meme_id = Meme.find_by_id(params[:comment][:meme_id])&.id
         comment.text = params[:comment][:text]
         if comment.save
-          flash[:success] = "Success: Posted a new comment!"
-          redirect "/memes/#{params[:comment][:meme_id]}"
+          flash[:success] = 'Success: Posted a new comment!'
         else
-          flash[:error] = "Error: Failed to save comment"
-          redirect "/memes/#{params[:comment][:meme_id]}"
+          flash[:error] = 'Error: Failed to save comment'
         end
+        redirect "/memes/#{params[:comment][:meme_id]}"
       else
-        flash[:error] = "Error: Failed getting current user"
+        flash[:error] = 'Error: Failed getting current user'
         redirect '/signin'
       end
     else
-      flash[:error] = "Error: You are not signed in"
+      flash[:error] = 'Error: You are not signed in'
+    end
+  end
+
+  delete '/comments/:id' do
+    if signed_in?
+      user = current_user
+      comment = Comment.find_by_id(params[:id])
+      meme_id = comment.meme.id
+      if user.id == comment.user.id || user.admin
+        if comment.destroy
+          flash[:success] = 'Success: Deleted comment!'
+        else
+          flash[:error] = 'Error: Failed to delete comment'
+        end
+        redirect "/memes/#{meme_id}"
+      else
+        flash[:error] = 'Error: You do not have the required permissions'
+      end
+    else
+      flash[:error] = 'Error: You are not signed in'
+      redirect '/signin'
     end
   end
 end
