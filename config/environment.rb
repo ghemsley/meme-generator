4fe3ef0574314f7cd555b8c2fd9a760866f2e476
ghemsley <ghemsley@protonmail.ch>
2021-02-09 04:54:39 -0600

    Update Procfile, database.yml, environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,14 @@ ENV['SINATRA_ENV'] ||= 'development'
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-set :database, { adapter: 'sqlite3', database: "db/#{ENV['SINATRA_ENV']}.sqlite3" }
+if ENV['SINATRA_ENV'] == 'development'
+  set :database, { adapter: 'sqlite3', database: "db/#{ENV['SINATRA_ENV']}.sqlite3" }
+end
 
 require_all './app'
 
 CarrierWave.configure do |config|
   config.root = './public/'
 end
+
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/memegenerator')
